facd41ed3912d5ed81c6866920da017783258f58
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-11-26 07:09:25 -0300

    fix litn

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -13,14 +13,22 @@ class ProcessJobs:
         return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
-        job_types = {job['job_type'] for job in self.jobs_list if 'job_type' in job}
+        job_types = {
+            job['job_type'] for job in self.jobs_list 
+            if 'job_type' in job
+        }
         return list(job_types)
 
-    def filter_by_multiple_criteria(self, filter_criteria: Dict[str, str]) -> List[Dict]:
+    def filter_by_multiple_criteria(self, 
+    filter_criteria: Dict[str, str]
+    ) -> List[Dict]:
 
         filtered_jobs = self.jobs_list
 
         for key, value in filter_criteria.items():
-            filtered_jobs = [job for job in filtered_jobs if job.get(key) == value]
+            filtered_jobs = [
+                job for job in filtered_jobs 
+                if job.get(key) == value
+            ]
 
         return filtered_jobs
