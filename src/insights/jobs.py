0c055aeff65dfee99079a2261dba6534e78a40e7
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-11-26 06:43:56 -0300

    req 03

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -13,7 +13,14 @@ class ProcessJobs:
         return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
-        pass
+        job_types = {job['job_type'] for job in self.jobs_list if 'job_type' in job}
+        return list(job_types)
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(self, filter_criteria: Dict[str, str]) -> List[Dict]:
+
+        filtered_jobs = self.jobs_list
+
+        for key, value in filter_criteria.items():
+            filtered_jobs = [job for job in filtered_jobs if job.get(key) == value]
+
+        return filtered_jobs
