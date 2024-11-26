6a9dab54c62a887d30332d99210d334db656b09e
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-11-26 14:40:29 -0300

    req 04

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,4 +7,8 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        all_industries = self.get_unique_industries()  
+
+        unique_industries = set(industry for industry in all_industries if industry)
+
+        return list(unique_industries)
